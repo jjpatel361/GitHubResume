98ce3996d41c891426e9e1ac5efb25c622ffb188
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2017-10-28 16:46:53 -0400

    React Init

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { ReduxPromise } from 'redux-promise';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
+const store = createStore(reducers, {}, applyMiddleware(ReduxPromise));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 registerServiceWorker();
